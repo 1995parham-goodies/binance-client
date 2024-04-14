c0d567ee8a0df4cf8766660c76f926a73e199b7f
Parham Alvani <parham.alvani@gmail.com>
2024-04-14 20:13:02 +0000

    feat: add more types of coins for download script

--- download-book-ticker.sh
+++ download-book-ticker.sh
@@ -4,24 +4,26 @@ touch links.txt
 echo "" >links.txt
 
 declare -a dates=(
-	"2024-02-17"
-	"2024-02-16"
-	"2024-02-15"
-	"2024-02-14"
-	"2024-02-13"
-	"2024-02-12"
-	"2024-02-11"
-	"2024-02-10"
-	"2024-02-09"
-	"2024-02-08"
-	"2024-02-07"
-	"2024-02-06"
-	"2024-02-05"
+	"2024-03-15"
+)
+
+declare -a coins=(
+	"BTC"
+	"ETH"
+	"BNB"
+	"SOL"
+	"XRP"
+	"DOGE"
+	"ADA"
+	"TON"
+	"AVA"
+	"DOT"
 )
 
 for date in "${dates[@]}"; do
-	curl -q -X POST https://www.binance.com/bapi/bigdata/v1/public/bigdata/finance/exchange/listDownloadData2 \
-		-d "{
+	for coin in "${coins[@]}"; do
+		curl -q -X POST https://www.binance.com/bapi/bigdata/v1/public/bigdata/finance/exchange/listDownloadData2 \
+			-d "{
     \"bizType\":\"FUTURES_UM\",
     \"productName\":\"bookTicker\",
     \"symbolRequestItems\":[
@@ -30,16 +32,10 @@ for date in "${dates[@]}"; do
       \"granularityList\":[],
       \"interval\":\"daily\",
       \"startDay\":\"$date\",
-      \"symbol\":\"BTCUSDT\"
-    },
-    {
-      \"endDay\":\"$date\",
-      \"granularityList\":[],
-      \"interval\":\"daily\",
-      \"startDay\":\"$date\",
-      \"symbol\":\"ETHUSDT\"
+      \"symbol\":\"${coin}USDT\"
     }
     ]}" -H 'Content-Type: application/json' | jq -r '.data.downloadItemList | .[] | .url' >>links.txt
+	done
 done
 
 aria2c -i links.txt
